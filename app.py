e0d56fb9c093ef295cd4661b959e8a0e27a7ad93
Rahul Krishna K <66860377+rahulkrishna146@users.noreply.github.com>
2024-07-10 15:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ boto_session = boto3.session.Session(
     aws_secret_access_key=aws_secret_access_key)
 
 # Create a Bedrock Runtime client in the AWS Region of your choice.
-client = boto_session.client("bedrock-runtime", region_name="us-west-2")
+client = boto_session.client("bedrock-runtime", region_name="us-east-1")
 
 st.title('Intelligent Correction')
 # initilaize session state 
@@ -29,30 +29,29 @@ if 'output_tok' not in st.session_state:
 grade = st.text_input('Class')
 max_marks = st.text_input('Maximum marks')
 partial_marks = st.toggle('Partial marks allowed')
-question_type = st.selectbox('Question type', ('Short Answer','Multi Part', 'Numerical word problem'))
+#question_type = st.selectbox('Question type', ('Short Answer','Multi Part', 'Numerical word problem'))
 subject = st.text_input('Enter the subject')
 chapter = st.text_input('Enter chapter name')
 question = st.text_area('Enter the question')
 accepted_answer = st.text_area('Enter accepted/best answer')
 grading_criteria = st.text_area('Describe grading criteria')
 student_answer = st.text_area('Enter student answer or upload an image')
-image_upload = st.file_uploader("Upload an image",type=['jpeg','png','jpg'])
+image_upload = st.file_uploader("Upload an image",type=['jpeg','png','jpg','pdf'])
 image_uploaded = ''# empty string
 # if image input:
 if image_upload is not None:
+    #student_answer = 'Please consider the image aswell text for student answer'
     student_answer = 'Please find the students handwritten answer as image'
     image_uploaded = 'from the image of the handwritten answer '
     image_bytes = image_upload.getvalue()
     encoded_image = base64.b64encode(image_bytes).decode("utf-8")
 
 #model selction
-model = st.selectbox('Choose model', ('Haiku','Sonnet','Opus'))
+model = st.selectbox('Choose model', ('Haiku','Sonnet3.5'))
 if model =='Haiku':
     model_id = "anthropic.claude-3-haiku-20240307-v1:0"
-elif model =='Sonnet':
-    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-elif model =='Opus':
-    model_id = "anthropic.claude-3-opus-20240229-v1:0"
+elif model =='Sonnet3.5':
+    model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0"
 
 #partial marks    
 if partial_marks:
@@ -62,7 +61,7 @@ else:
 
 # assemble prompt
 prompt = PromptTemplate.from_template("""You are a grade {grade} school teacher teaching {subject}. Your task is to evaluate a student’s answer against an accepted answer and give marks out of {max_marks} based on the optionally given grading criteria and standards.
-This is a {question_type} question from the chapter {chapter}. {partial}
+This is a question from the chapter {chapter}. {partial}
 <question>{question}</question> 
 <acceted_answer>{accepted_answer}</acceted_answer>
 <grading_criteria>{grading_criteria}</grading_criteria>
@@ -71,10 +70,10 @@ Please evaluate the student’s answer {image_uploaded}and provide most concise 
 - student’s marks inside <marks> </marks> tags 
 - reason for your grading inside <reason> </reason> tags""")
 
-model_prompt = prompt.invoke({"grade":grade, "subject": subject, "max_marks": max_marks, "question_type": question_type,"chapter": chapter, "partial": partial, "question": question, "accepted_answer": accepted_answer, "grading_criteria": grading_criteria, "student_answer": student_answer, "image_uploaded": image_uploaded})
+model_prompt = prompt.invoke({"grade":grade, "subject": subject, "max_marks": max_marks,"chapter": chapter, "partial": partial, "question": question, "accepted_answer": accepted_answer, "grading_criteria": grading_criteria, "student_answer": student_answer, "image_uploaded": image_uploaded})
 
-#if st.button(label = 'view prompt'):
-    #st.write(f"Prompt: {model_prompt.text}")
+if st.button(label = 'view prompt'):
+    st.write(f"Prompt: {model_prompt.text}")
 
 if st.button(label = 'Evaluate student answer'):
     # Format the request payload using the model's native structure.
@@ -124,7 +123,6 @@ if st.button(label = 'Evaluate student answer'):
 
     st.session_state.input_tok = input_tok
     st.session_state.output_tok = output_tok
-
     # Extract and print the response text.
     response_text = model_response["content"][0]["text"]
     marks_response = re.search(r'<marks>(.*?)</marks>', response_text).group(1)
@@ -139,11 +137,10 @@ if st.button(label = 'Analyse cost of evaluation'):
     st.write(f"model used: {model}")
     if model=="Haiku":
         cost = st.session_state.input_tok*0.00025/1000 + st.session_state.output_tok*0.00125/1000
-    elif model =='Sonnet':
+    elif model =='Sonnet3.5':
         cost = st.session_state.input_tok*0.003/1000 + st.session_state.output_tok*0.015/1000
-    elif model == 'Opus':
-        cost = st.session_state.input_tok*0.015/1000 + st.session_state.output_tok*0.075/1000
     st.write(f"Cost of evaluating : {cost} USD")
-
     
+
+
 
